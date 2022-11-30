721646cb89da054078d6e11f0da8e6305f14077f
NicNel <100981393+NicNel@users.noreply.github.com>
2022-11-30 09:03:44 +0300

    Add simple sort

--- src/TMath/TCommon.c
+++ src/TMath/TCommon.c
@@ -3,6 +3,31 @@
 #include <math.h>
 
 float DegToRad(float deg) { return M_PI * deg / 180.0f; }
-float RadToDeg(float rad) { rad* (180.0f / M_PI); }
+float RadToDeg(float rad) { return rad * (180.0f / M_PI); }
 float Lerp(float a, float b, float t) { return a - (a * t) + (b * t); }
 float InvLerp(float a, float b, float t) { 	return (t - a) / (b - a); }
+
+float rngFloat() { return  (float)(rand()) / (float)(RAND_MAX); }
+int rngInt(int min, int max) { int randNum = rand() % (max - min + 1) + min; return randNum; }
+
+void InsertionSort(float *values, int n) {
+	for (size_t i = 1; i < n; ++i) {
+		float x = values[i];
+		size_t j = i;
+		while (j > 0 && values[j - 1] > x) {
+			values[j] = values[j - 1];
+			--j;
+		}
+		values[j] = x;
+	}
+}
+
+float Mean(float *items, int n)
+{
+	float sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += items[i];
+	}
+	return sum / n;
+}
--- src/TMath/TCommon.h
+++ src/TMath/TCommon.h
@@ -5,12 +5,19 @@
 extern "C" {
 #endif 
 
+#include <stdlib.h>
 # define M_PI 3.14159265358979323846 //pi 
 
 float DegToRad(float deg);
 float RadToDeg(float rad);
 float Lerp(float a, float b, float t);
 float InvLerp(float a, float b, float t);
+
+float rngFloat();
+int rngInt(int min, int max);
+
+void InsertionSort(float *values, int n);
+float Mean(float *items, int n);
 
 #ifdef __cplusplus
 }
