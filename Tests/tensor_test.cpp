f9c504ba9dff98d7a2d898a3ae9a1f9cd9005e65
NicNel <100981393+NicNel@users.noreply.github.com>
2023-05-03 04:43:02 +0300

    Add weights init test

--- Tests/tensor_test.cpp
+++ Tests/tensor_test.cpp
@@ -1,4 +1,6 @@
 #include "Tensor.h"
+#include "TWeightsInit.h"
+#include "Utils.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -48,8 +50,47 @@ void Tensor_ToJSON_Test()
 	Tensor_Free(jten);
 }
 
+void WeightsInitTest()
+{
+	int count = 10000;
+	float min = FLT_MAX, max = FLT_MIN, sum = 0;
+	printf("xavier\n");
+	for (size_t i = 0; i < count; i++)
+	{
+		float f = xavier_rand(10);
+		//printf("%f, ", f);
+		min = f < min ? f : min;
+		max = f > max ? f : max;
+		sum += f;
+	}
+	printf("\nmin val:%f max val:%f, mean:%f\n", min, max, sum / (float)count);
+	min = FLT_MAX, max = FLT_MIN, sum = 0;
+	printf("\nxavier normalized\n");
+	for (size_t i = 0; i < count; i++)
+	{
+		float f = xavier_norm_rand(10, 20);
+		//printf("%f, ", f);
+		min = f < min ? f : min;
+		max = f > max ? f : max;
+		sum += f;
+	}
+	printf("\nmin val:%f max val:%f, mean:%f\n", min, max, sum / (float)count);
+	min = FLT_MAX, max = FLT_MIN, sum = 0;
+	printf("\nhe distribution\n");
+	for (size_t i = 0; i < count; i++)
+	{
+		float f = he_rand(10);
+		//printf("%f, ", f);
+		min = f < min ? f : min;
+		max = f > max ? f : max;
+		sum += f;
+	}
+	printf("\nmin val:%f max val:%f, mean:%f\n", min, max, sum / (float)count);
+}
+
 int main() 
 {
 	Tensor_ToJSON_Test();
+	WeightsInitTest();
 	return 0;
 }
