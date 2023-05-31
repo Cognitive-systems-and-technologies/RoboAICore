3d7c160fcfe14a0bd56d910aa1f8e8aa4a32c42d
NicNel <100981393+NicNel@users.noreply.github.com>
2023-05-31 10:13:36 +0300

    Update messages

--- src/message.c
+++ src/message.c
@@ -28,6 +28,22 @@ Message ParseMessage(const char* str)
 	return m;
 }
 
+AgentInfo ParseAInfo(cJSON *elem) 
+{
+	AgentInfo ai;
+	cJSON* name = cJSON_GetObjectItem(elem, "name");
+	cJSON* mac = cJSON_GetObjectItem(elem, "mac");
+	cJSON* ip = cJSON_GetObjectItem(elem, "ip");
+	cJSON* port = cJSON_GetObjectItem(elem, "port");
+
+	ai.name = strCopy(name->valuestring);
+	ai.mac = strCopy(mac->valuestring);
+	ai.ip = strCopy(ip->valuestring);
+	ai.port = port->valueint;
+
+	return ai;
+}
+
 cJSON* cJsonFromMessage(Message* m, int parse_body)
 {
 	cJSON* node = cJSON_CreateObject();
@@ -48,10 +64,27 @@ cJSON* cJsonMessage(const char *role,const char *type,const char *message)
 	return node;
 }
 
+cJSON* cJsonAInfo(const char* name, const char* mac, const char* ip, int port)
+{
+	cJSON* node = cJSON_CreateObject();
+	cJSON_AddStringToObject(node, "name", name);
+	cJSON_AddStringToObject(node, "mac", mac);
+	cJSON_AddStringToObject(node, "ip", ip);
+	cJSON_AddNumberToObject(node, "port", port);
+	return node;
+}
+
 void FreeMessage(Message* m) 
 {
 	free(m->role);
 	free(m->type);
 	free(m->message);
 	free(m->body);
+}
+
+void FreeAInfo(AgentInfo* ai)
+{
+	free(ai->name);
+	free(ai->mac);
+	free(ai->ip);
 }
--- src/message.h
+++ src/message.h
@@ -18,11 +18,21 @@ typedef struct Message
 	const char *body;
 }Message;
 
+typedef struct AgentInfo {
+    const char *name;
+	const char* mac;
+	const char* ip;
+    int port;
+}AgentInfo;
+
 Message ParseMessage(const char* str);
 cJSON* cJsonFromMessage(Message *m, int parse_body);
 void FreeMessage(Message *m);
 char* strCopy(const char* str);
 cJSON* cJsonMessage(const char* role, const char* type, const char* message);
+cJSON* cJsonAInfo(const char* name, const char* mac, const char* ip, int port);
+AgentInfo ParseAInfo(cJSON* elem);
+void FreeAInfo(AgentInfo* ai);
 #ifdef __cplusplus
 }
 #endif
