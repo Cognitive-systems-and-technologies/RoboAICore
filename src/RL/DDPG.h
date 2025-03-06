66dd38ec9aeb16701ffa848935c1289382170533
NicNel <100981393+NicNel@users.noreply.github.com>
2025-03-07 02:32:57 +0300

    Update DDPG

--- src/RL/DDPG.h
+++ src/RL/DDPG.h
@@ -30,18 +30,20 @@ typedef struct DDPG
 	float tau;
 	shape input_shape;
 	int num_outputs;
+	int update_frq;
+	OrnsteinUhlenbeckNoise noise;
 	OptParams par;
 }DDPG;
 
-void soft_update(dList* tp, dList*sp, float tau);
+void DDPGsoft_update(dList* tp, dList*sp, float tau);
 DDPG* DDPG_Create(shape state_shape, int n_acts);
 Tensor DDPG_Forward(DDPG *brain, Tensor *state);
 Tensor DDPG_SelectAction(DDPG* brain, Tensor* state, float eps);
 
-float DDPG_TrainTrace(DDPG* brain, Tensor* states, Tensor* probs, float* rewards, int n, int iter);
+float DDPG_TrainTrace(DDPG* brain, Tensor* states, Tensor* last_state, Tensor* probs, float* rewards, int n, int iter);
 
-void copy_params(Model* dest_m, Model* src_m);
-void SoftUpdate_Targets(DDPG* brain);
+void DDPGcopy_params(Model* dest_m, Model* src_m);
+void DDPGSoftUpdate_Targets(DDPG* brain);
 #ifdef __cplusplus
 }
 #endif
